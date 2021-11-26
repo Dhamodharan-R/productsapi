954d8a2a42f7a1947b86281be8403364de76e6df
Dhamodharan-R <82367414+Dhamodharan-R@users.noreply.github.com>
2021-11-26 16:18:52 +0530

    Update mongo.js

--- Shared/mongo.js
+++ Shared/mongo.js
@@ -1,14 +1,17 @@
 const {MongoClient} = require("mongodb");
 
-const client = new MongoClient(process.env.URLM);
+const DBNAME="products"
+const URLM="mongodb+srv://dbUser:admin123@cluster0.82h79.mongodb.net"
+
+const client = new MongoClient(URLM);
 
 module.exports = {
     db : null ,
     async connect () {
         await client.connect();
-        console.log(`Connected to url ${process.env.URLM}`);
-        this.db = client.db(process.env.DBNAME);
-        console.log(`Selected to database ${process.env.DBNAME}`)
+        console.log(`Connected to url ${URLM}`);
+        this.db = client.db(DBNAME);
+        console.log(`Selected to database ${DBNAME}`)
     }
 
 }
